0d973b62a4e50f3b3e5ca8286e490931b2cc6d2c
Martin Hoefling <martin.hoefling@gmx.de>
2016-12-09 16:24:00 +0100

    Add support for gzipped and zipped files

--- report/reader.go
+++ report/reader.go
@@ -11,6 +11,15 @@ import (
 	"strings"
 	"time"
 
+	"compress/gzip"
+
+	"io"
+	"os"
+
+	"archive/zip"
+
+	"bytes"
+
 	"github.com/martinhoefling/go-dmarc-report/utils"
 )
 
@@ -76,10 +85,33 @@ func ReadReports(reportPath string) map[string][]Feedback {
 		filePath, err := filepath.Abs(path.Join(reportPath, f.Name()))
 		utils.CheckError(err)
 		fmt.Printf("Loading %s\n", filePath)
-		fileBytes, err := ioutil.ReadFile(filePath)
+		var file io.Reader
+		file, err = os.Open(filePath)
 		utils.CheckError(err)
-
+		if strings.HasSuffix(filePath, ".gz") {
+			file, err = gzip.NewReader(file)
+			utils.CheckError(err)
+		}
+		if strings.HasSuffix(filePath, ".zip") {
+			bt, err2 := ioutil.ReadAll(file)
+			utils.CheckError(err2)
+			r, err2 := zip.NewReader(bytes.NewReader(bt), int64(len(bt)))
+			utils.CheckError(err2)
+			if len(r.File) != 1 {
+				fmt.Printf("Not exactly one file in zip %s", filePath)
+				continue
+			}
+			f := r.File[0]
+			if !strings.HasSuffix(f.Name, ".xml") {
+				fmt.Printf("Not an xml file in zip %s", filePath)
+				continue
+			}
+			file, err2 = f.Open()
+			utils.CheckError(err2)
+		}
 		r, _ := regexp.Compile("(?is:<feedback.*</feedback>)")
+		fileBytes, err := ioutil.ReadAll(file)
+		utils.CheckError(err)
 		validBytes := r.Find(fileBytes)
 		var q Query
 		utils.CheckError(xml.Unmarshal(validBytes, &q.Feedback))
